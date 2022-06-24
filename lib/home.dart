6657a25ae9eeca5bf6cec43a3cff59518a9f3790
LENOVO <krishgelani6664@gmail.com>
2022-06-24 18:14:09 +0530

    -24-06-2022

--- lib/home.dart
+++ lib/home.dart
@@ -26,53 +26,54 @@ class _homeState extends State<home> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          backgroundColor: Colors.black87,
-          appBar: AppBar(
-            backgroundColor: Colors.deepOrange,
-            title: Text("Contacts"),
-            actions: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-              SizedBox(
-                width: 5,
-              ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.sort)),
-              SizedBox(
-                width: 5,
-              ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.filter_list)),
-              SizedBox(
-                width: 5,
-              ),
-              PopupMenuButton(
-                  itemBuilder: (context) {
-                    return [
-                      PopupMenuItem(
-                        child: Text("Add Contact"),
-                        onTap: () {
-                          //dialog();
-                        },
-                      ),
-                      PopupMenuItem(
-                        child: Text("Delete All Contact"),
-                        onTap: () {
-                          setState(() {
-                            name.clear();
-                            contact.clear();
-                            img.clear();
-                          });
-                        },
-                      )
-                    ];
-                  },
-                  child: Icon(Icons.more_vert)),
-            ],
-          ),
-          body: Stack(
-            children: [
-              ListView.builder(
-                itemCount: name.length,
-                itemBuilder: (context, index) {
-                  return Stack(children: [
+        backgroundColor: Colors.black87,
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text("Contacts"),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            SizedBox(
+              width: 5,
+            ),
+            IconButton(onPressed: () {}, icon: Icon(Icons.sort)),
+            SizedBox(
+              width: 5,
+            ),
+            IconButton(onPressed: () {}, icon: Icon(Icons.filter_list)),
+            SizedBox(
+              width: 5,
+            ),
+            PopupMenuButton(
+                itemBuilder: (context) {
+                  return [
+                    PopupMenuItem(
+                      child: Text("Add Contact"),
+                      onTap: () {},
+                    ),
+                    PopupMenuItem(
+                      child: Text("Delete All Contact"),
+                      onTap: () {
+                        setState(() {
+                          name.clear();
+                          contact.clear();
+                          img.clear();
+                        });
+                      },
+                    )
+                  ];
+                },
+                child: Icon(Icons.more_vert)),
+          ],
+        ),
+
+
+        body: Stack(
+          children: [
+            ListView.builder(
+              itemCount: name.length,
+              itemBuilder: (context, index) {
+                return Stack(
+                  children: [
                     Padding(
                       padding: const EdgeInsets.all(5.0),
                       child: Card(
@@ -80,9 +81,9 @@ class _homeState extends State<home> {
                         child: ElevatedButton(
                           onPressed: () {
                             Model m1 = Model(
-                                name: txtname.text,
-                                number: txtnumber.text,
-                                photo: f1.path);
+                                name: name[index],
+                                number: contact[index],
+                                photo: img[index]);
 
                             Navigator.pushNamed(context, '/cd', arguments: m1);
                           },
@@ -97,6 +98,7 @@ class _homeState extends State<home> {
                                   height: 60,
                                   width: 60,
                                   child: CircleAvatar(
+                                      backgroundColor: Colors.black87,
                                       backgroundImage: FileImage(
                                           File(img[index].toString()))),
                                 ),
@@ -114,104 +116,111 @@ class _homeState extends State<home> {
                         ),
                       ),
                     ),
-                  ]);
-                },
+                  ],
+                );
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.deepOrange,
+                  onPressed: () {
+                    dialog();
+                    txtname.clear();
+                    txtnumber.clear();
+                    f1 = File("");
+                  },
+                  child: Icon(Icons.add),
+                ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        dialog();
-                        txtname.clear();
-                        txtnumber.clear();
-                        f1 = File("");
-                      },
-                      child: Icon(Icons.add),
-                    )),
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   void dialog() {
     showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            content: Column(
-              children: [
-                GestureDetector(
-                  onTap: () async {
-                    ImagePicker ipick = ImagePicker();
-                    XFile? f2 =
-                        await ipick.pickImage(source: ImageSource.gallery);
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Column(
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  ImagePicker ipick = ImagePicker();
+                  XFile? f2 =
+                      await ipick.pickImage(source: ImageSource.gallery);
 
-                    setState(() {
-                      f1 = File(f2!.path);
-                    });
-                  },
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    child: CircleAvatar(
-                      backgroundImage: FileImage(f1),
-                    ),
+                  setState(() {
+                    f1 = File(f2!.path);
+                  });
+                },
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  child: CircleAvatar(
+                    backgroundImage: FileImage(f1),
                   ),
                 ),
-                SizedBox(
-                  height: 20,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: txtname,
+                decoration: InputDecoration(
+                  label: Text("Name"),
+                  enabledBorder: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
                 ),
-                TextField(
-                  controller: txtname,
-                  decoration: InputDecoration(
-                      label: Text("Name"),
-                      enabledBorder: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue))),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  controller: txtnumber,
-                  decoration: InputDecoration(
-                      label: Text("Mobile Number"),
-                      enabledBorder: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue))),
-                )
-              ],
-            ),
-            actions: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Cancel")),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      cname = txtname.text;
-                      cnumber = txtnumber.text;
-                      name.add(cname);
-                      contact.add(cnumber);
-                      img.add(f1.path);
-                    });
-                    Navigator.pop(context);
-                    snack();
-                  },
-                  child: Text("Save")),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: txtnumber,
+                decoration: InputDecoration(
+                    label: Text("Mobile Number"),
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue))),
+              )
             ],
-          );
-        });
+          ),
+          actions: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    cname = txtname.text;
+                    cnumber = txtnumber.text;
+                    name.add(cname);
+                    contact.add(cnumber);
+                    img.add(f1.path);
+                  });
+                  Navigator.pop(context);
+                  snack();
+                },
+                child: Text("Save")),
+          ],
+        );
+      },
+    );
   }
 
-  void snack()
-  {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Contact Saved")));
+  void snack() {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Contact Saved")));
   }
 }
